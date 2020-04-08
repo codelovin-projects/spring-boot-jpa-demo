617b7158eb86d5eb8636e5df3b463738b898fbc9
Arita Malik <hello@coderport.com>
2020-04-08 20:10:33 +0530

    initial commit

---
+++ src/test/java/com/codelovin/springboot/jpademo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelovin.springboot.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
